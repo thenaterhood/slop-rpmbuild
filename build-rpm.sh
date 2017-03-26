26e66da7fa1555a674d88d6708ac6ab4baa36f9e
Nate Levesque <nate@thenaterhood.com>
2017-03-26 15:59:45 -0400

    Add install builddeps

--- build-rpm.sh
+++ build-rpm.sh
@@ -6,6 +6,9 @@ mkdir -p ${RPMBUILD_DIR}
 echo "Downloading slop version ${VERSION}..."
 wget https://github.com/naelstrof/slop/archive/v${VERSION}.tar.gz -O ${RPMBUILD_DIR}/SOURCES/slop-${VERSION}.tar.gz
 
+echo "Installing build dependencies..."
+dnf builddep SPECS/slop.spec
+
 echo "Building RPM..."
 rpmbuild -ba SPECS/slop.spec
 
